ab7aefcc1de63f22180c6b411f6683c01c7f82ac
Niaso <39199340+Zpk2@users.noreply.github.com>
2021-04-08 18:16:32 +0200

    🚧 Commit

---
+++ Simple RPC.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace Simple_RPC
+{
+    partial class simplerpc
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(simplerpc));
+            this.smallimgkeytxt = new System.Windows.Forms.TextBox();
+            this.largeimgkeytxt = new System.Windows.Forms.TextBox();
+            this.largeimgtxt = new System.Windows.Forms.TextBox();
+            this.smallimgtxt = new System.Windows.Forms.TextBox();
+            this.statetxt = new System.Windows.Forms.TextBox();
+            this.dettxt = new System.Windows.Forms.TextBox();
+            this.idtxt = new System.Windows.Forms.TextBox();
+            this.refreshBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // smallimgkeytxt
+            // 
+            this.smallimgkeytxt.Location = new System.Drawing.Point(154, 150);
+            this.smallimgkeytxt.Name = "smallimgkeytxt";
+            this.smallimgkeytxt.Size = new System.Drawing.Size(289, 20);
+            this.smallimgkeytxt.TabIndex = 0;
+            // 
+            // largeimgkeytxt
+            // 
+            this.largeimgkeytxt.Location = new System.Drawing.Point(154, 124);
+            this.largeimgkeytxt.Name = "largeimgkeytxt";
+            this.largeimgkeytxt.Size = new System.Drawing.Size(289, 20);
+            this.largeimgkeytxt.TabIndex = 1;
+            // 
+            // largeimgtxt
+            // 
+            this.largeimgtxt.Location = new System.Drawing.Point(154, 176);
+            this.largeimgtxt.Name = "largeimgtxt";
+            this.largeimgtxt.Size = new System.Drawing.Size(289, 20);
+            this.largeimgtxt.TabIndex = 2;
+            // 
+            // smallimgtxt
+            // 
+            this.smallimgtxt.Location = new System.Drawing.Point(154, 202);
+            this.smallimgtxt.Name = "smallimgtxt";
+            this.smallimgtxt.Size = new System.Drawing.Size(289, 20);
+            this.smallimgtxt.TabIndex = 3;
+            // 
+            // statetxt
+            // 
+            this.statetxt.Location = new System.Drawing.Point(154, 98);
+            this.statetxt.Name = "statetxt";
+            this.statetxt.Size = new System.Drawing.Size(289, 20);
+            this.statetxt.TabIndex = 4;
+            // 
+            // dettxt
+            // 
+            this.dettxt.Location = new System.Drawing.Point(154, 72);
+            this.dettxt.Name = "dettxt";
+            this.dettxt.Size = new System.Drawing.Size(289, 20);
+            this.dettxt.TabIndex = 5;
+            // 
+            // idtxt
+            // 
+            this.idtxt.Location = new System.Drawing.Point(154, 46);
+            this.idtxt.Name = "idtxt";
+            this.idtxt.Size = new System.Drawing.Size(289, 20);
+            this.idtxt.TabIndex = 6;
+            // 
+            // refreshBtn
+            // 
+            this.refreshBtn.Location = new System.Drawing.Point(472, 201);
+            this.refreshBtn.Name = "refreshBtn";
+            this.refreshBtn.Size = new System.Drawing.Size(107, 46);
+            this.refreshBtn.TabIndex = 7;
+            this.refreshBtn.Text = "Actualiser";
+            this.refreshBtn.UseVisualStyleBackColor = true;
+            this.refreshBtn.Click += new System.EventHandler(this.refreshBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Application ID :";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(532, 9);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(47, 13);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Mon site";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Details :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "État :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Clé grande image :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Clé petite image :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 176);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Text grande image :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 202);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Text petite image :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(228, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(133, 28);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Simple RPC";
+            // 
+            // simplerpc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 259);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.refreshBtn);
+            this.Controls.Add(this.idtxt);
+            this.Controls.Add(this.dettxt);
+            this.Controls.Add(this.statetxt);
+            this.Controls.Add(this.smallimgtxt);
+            this.Controls.Add(this.largeimgtxt);
+            this.Controls.Add(this.largeimgkeytxt);
+            this.Controls.Add(this.smallimgkeytxt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "simplerpc";
+            this.Text = "Simple RPC";
+            this.Load += new System.EventHandler(this.simplerpc_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox smallimgkeytxt;
+        private System.Windows.Forms.TextBox largeimgkeytxt;
+        private System.Windows.Forms.TextBox largeimgtxt;
+        private System.Windows.Forms.TextBox smallimgtxt;
+        private System.Windows.Forms.TextBox statetxt;
+        private System.Windows.Forms.TextBox dettxt;
+        private System.Windows.Forms.TextBox idtxt;
+        private System.Windows.Forms.Button refreshBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Simple RPC.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DiscordRpcDemo;
+
+namespace Simple_RPC
+{
+    public partial class simplerpc : Form
+    {
+        private DiscordRpc.EventHandlers handlers;
+        private DiscordRpc.RichPresence presence;
+        public simplerpc()
+        {
+            InitializeComponent();
+        }
+
+        private void simplerpc_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void refreshBtn_Click(object sender, EventArgs e)
+        {
+            this.handlers = default(DiscordRpc.EventHandlers);
+            DiscordRpc.Initialize(idtxt.Text, ref this.handlers, true, null);
+            this.handlers = default(DiscordRpc.EventHandlers);
+            DiscordRpc.Initialize(idtxt.Text, ref this.handlers, true, null);
+            this.presence.state = statetxt.Text;
+            this.presence.details = dettxt.Text;
+            this.presence.largeImageKey = largeimgkeytxt.Text;
+            this.presence.smallImageKey = smallimgkeytxt.Text;
+            this.presence.largeImageText = largeimgtxt.Text;
+            this.presence.smallImageText = smallimgtxt.Text;
+            DiscordRpc.UpdatePresence(ref this.presence);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://niaso.cf");
+        }
+    }
+}
